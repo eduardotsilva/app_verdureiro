76038e156d86ceae899dd750fb7a0e5897fc3662
Eduardo Tadeu <eduardo.tsilva@hotmail.com>
2023-02-26 16:03:07 -0400

    Criando e adicionando cores customizadas

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -2,13 +2,15 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:verdureiro/src/auth/components/custom_text_field.dart';
 
+import '../config/custom_colors.dart';
+
 class SignInScreen extends StatelessWidget {
   const SignInScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
+      backgroundColor: CustomColors.customSwatchColor,
       body: Column(
         children: [
           Expanded(
@@ -16,20 +18,21 @@ class SignInScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 //Nome do app
-                const Text.rich(
+                Text.rich(
                   TextSpan(
-                    style: TextStyle(
+                    style: const TextStyle(
                       fontSize: 40,
                     ),
                     children: [
-                      TextSpan(
+                      const TextSpan(
                         text: 'Green',
                         style: TextStyle(
                             color: Colors.white, fontWeight: FontWeight.bold),
                       ),
                       TextSpan(
                         text: 'grocer',
-                        style: TextStyle(color: Colors.redAccent),
+                        style:
+                            TextStyle(color: CustomColors.customConstrantColor),
                       ),
                     ],
                   ),
@@ -97,9 +100,10 @@ class SignInScreen extends StatelessWidget {
                   alignment: Alignment.centerRight,
                   child: TextButton(
                     onPressed: () {},
-                    child: const Text(
+                    child: Text(
                       'Esqueceu a senha?',
-                      style: TextStyle(color: Colors.red),
+                      style:
+                          TextStyle(color: CustomColors.customConstrantColor),
                     ),
                   ),
                 ),
